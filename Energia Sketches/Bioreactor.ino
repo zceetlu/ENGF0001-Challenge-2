c58d126600ad72ad3081956271cd07399caea0b4
Ernzst <55922827+Ernzst@users.noreply.github.com>
2019-12-05 10:05:42 +0000

    Update and rename final.ino to Bioreactor.ino

--- Energia Sketches/Bioreactor.ino
+++ Energia Sketches/Bioreactor.ino
@@ -17,15 +17,61 @@ int heater = 7; //temperature vars
 int Pot = 0;
 int R = 10000;
 int Setpoint = 30; //should be 28-33
+ 
+//stirring vars
+int readCurrent = 35;//read Iout from photo-interrupter sq wave
+int controlMotor = 32 ; //output PWM to motot
+int Vcc1 = 40; // interupter
+int Vcc2 = 39; // LED on interrupter
+int GND1 = 18;
+int GND2 = 17;
+
+int motorSpeed = 255;
+int recordTime =0;
+int recordRotation=0;
+int lastCurrent = 0;
+int newCurrent=0;
+
+int lowerRange = 200; // brm lower range for the motor = 600
+int upperRange = 1000; // brm upper range for the motor = 620
 
 void setup() {
     pinMode(acid_LED, OUTPUT); //pH setup
     pinMode(base_LED, OUTPUT);
     pinMode(pHpin, INPUT);
-    Serial.begin(9600);
 
     pinMode(Pot, INPUT); //temperature setup
     pinMode(heater, OUTPUT);
+    
+    pinMode(readCurrent,INPUT); //stirring setup
+    pinMode(controlMotor,OUTPUT);
+    pinMode(Vcc1,OUTPUT);
+    pinMode(Vcc2,OUTPUT);
+    pinMode(GND1,OUTPUT);
+    pinMode(GND2,OUTPUT);
+    Serial.begin(9600);
+}
+
+void update_variables() {
+    if (Serial.available() > 0 && Serial.read() == '<')
+    {
+        char input[25];
+        int charsRead = Serial.readBytesUntil('>', input, 25);
+        input[charsRead] = '\0';
+        char* subsystem = strtok(input, ",");
+        char* ptr2 = strtok(NULL, ",");
+        double value = atof(ptr2);
+        if (strcmp("pH", subsystem)==1) {
+            pH = value;
+        }
+        else if (strcmp("temperature", subsystem)==1) {
+            temperature = value;
+        }
+        else if (strcmp("speed", subsystem)==1) {
+            motorspeed = value;
+        }
+    }
+    delay(200);
 }
 
 void read_pH()
@@ -58,10 +104,16 @@ void read_temperature()
 
 void read_speed()
 {}
+
 void loop() {
-    read_pH();
-    read_temperature();
-    read_speed();
+    if (Serial.available() > 0) {
+        update_variables();
+    }
+    else {
+        read_pH();
+        read_temperature();
+        read_speed();
+    }
 
     Serial.print("<pH,");
     Serial.print(pH);
