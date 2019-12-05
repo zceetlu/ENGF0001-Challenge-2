ebf32df8a20f6c34579f2dfcae1beb15549c34e8
Karan Chawla <55800358+1kc2@users.noreply.github.com>
2019-12-05 09:53:16 +0000

    Update temp.ino

--- Energia Sketches/temp.ino
+++ Energia Sketches/temp.ino
@@ -1,53 +1,47 @@
 int heater = 7;
-
 int Pot = 0;
-int R=10000;
+int R = 10000;
 int Setpoint = 30;
 
-
-
-void setup() {
-
-  // put your setup code here, to run once:
-
+void setup()
+{
     pinMode(Pot, INPUT);
-
     pinMode(heater, OUTPUT);
-
     Serial.begin(9600);
-
-
-
 }
 
-void loop() {
-
-  // put your main code here, to run repeatedly:
-
-   int Pot = analogRead(A4) ;
-    Serial.println("pot reading");
-    Serial.println(Pot);
-   float resistance=(Pot*R)/(1023-Pot);
-    Serial.println("Resistance reading");
-    Serial.println(resistance);
-   float T=-0.0027*resistance+52;
-    Serial.println("Temperature reading");
-    Serial.println(T);
-    if(T < Setpoint+0.5){
-
-            digitalWrite(heater, HIGH);
-    }
-    else if(T > Setpoint+0.5){
-
+void loop()
+{
+    int Pot = analogRead(A4) ;
+    float resistance=(Pot*R)/(1023-Pot);
+    float T=-0.0027*resistance+52;
+    Serial.print("<temperature,");
+    Serial.print(T);
+    Serial.println(">");
+    
+    if(T < Setpoint+0.5)
+        digitalWrite(heater, HIGH);
+    
+    else if(T > Setpoint+0.5)
         digitalWrite(heater, LOW);
-
+    
+    else if(Setpoint-0.5 < T < Setpoint+0.5)
+        digitalWrite(heater, LOW);
+    
+    if (Serial.available() > 0 && Serial.read() == '<')
+    {
+        char input[25];
+        int charsRead = Serial.readBytesUntil('>', input, 25);
+        input[charsRead] = '\0';
+        char* subsystem = strtok(input, ",");
+        char* ptr2 = strtok(NULL, ",");
+        Serial.print(ptr);
+        Serial.print(" ");
+        Serial.println(ptr2);
+        double val = atof(ptr2);
+        
+        if (val != Setpoint)
+            Setpoint = val;
     }
-    else if(Setpoint-0.5 < T < Setpoint+0.5){
-
-            digitalWrite(heater, LOW);
-
-        }
-
-    delay(1000);
-
+    delay(200);
 }
